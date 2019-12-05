938e4bd4956b685adb6ecc952ee67e768d7e094b
zierp <zierp@LAPTOP-4TV42EQM>
2019-12-05 11:58:49 +0100

    v1.1

--- default/nyc.py
+++ default/nyc.py
@@ -3,36 +3,40 @@ Created on 4 dic 2019
 
 @author: zierp
 '''
-import csv
 import numpy as np
 from sklearn.model_selection._split import train_test_split
 from sklearn.ensemble.forest import RandomForestRegressor
 from sklearn.metrics.regression import r2_score
-from sklearn.preprocessing import LabelEncoder
+import pandas as pd
 
 #ex1
 X = []
-y = []
 """
  which attribute (or set of attributes) you think could drive the price per night the most?
  can you detect any irregularity in any attribute distribution?
  if your regression model will fit on numerical data only, how could you handle categorical
 attributes?
 """
-with open("development.csv", encoding="utf-8") as file:
-    for row in csv.reader(file, delimiter=","):
-            X.append( [row[2], row[4], row[5] ,row[8], row[10], row[11] ,row[14] ,row[15] ])
-            y.append(row[9])
-   
-X.pop(0)
-y.pop(0)  
+
+# Load data
+X = pd.read_csv('development.csv')
+# Discard rows with price = 0
+X = X[X.price > 0 ]
+
+"""Drop all useless columns and rows containing NaN"""
+X = X.dropna()
+# Build y containing price
+y = np.array(X.loc[:,['price']])
+X = X.drop(columns=['id','name','host_name','latitude','longitude','price','last_review'])
+""""""
+
+# Encode categorical attributes
+X = pd.get_dummies(X, columns=['host_id','neighbourhood_group','neighbourhood','room_type'], drop_first=True)
 
 #Regression
-X = np.array(X)
-y = np.array(y)
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=30, random_state=42, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, random_state=42, shuffle=True)
 reg = RandomForestRegressor(n_estimators=100)
 reg.fit(X_train,y_train)
 y_pred = reg.predict(X_test)
-print("Accuracy f3: ",r2_score(y_pred, y_test))
+print("Accuracy: ",r2_score(y_pred, y_test))
 
